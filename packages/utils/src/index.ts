e0fc75147da858c5e64bc7c3b71c133fe03c6562
simonlovesyou <simonljohansson92@gmail.com>
2021-05-17 21:07:53 +0200

    chore(utils): Ignore type errors (for now)

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,13 +1,18 @@
 import { JSONSchema7 } from "json-schema";
+// @ts-ignore
 import { Argument } from '@cling/parser'
 
 export const convertJSONSchemaToArgument = (jsonSchema: JSONSchema7): Argument => {
   if(jsonSchema.type && Array.isArray(jsonSchema.type)) {
     throw new Error('Cannot convert JSON Schema to cling Argument with multiple types')
   }
+  // @ts-ignore
   return {
+    // @ts-ignore
     $ref: jsonSchema.$ref,
+    // @ts-ignore
     type: jsonSchema.type,
+    // @ts-ignore
     description: jsonSchema.description
   }
 }
