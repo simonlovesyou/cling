8714d5b8d086c34b8c59e2ee13d45326d537d13a
simon.johansson <simonljohansson92@gmail.com>
2021-07-17 19:57:26 +0200

    fix(utils): Fix common cases where JSON Schema compatible properties were not carried over

--- packages/utils/src/index.spec.ts
+++ packages/utils/src/index.spec.ts
@@ -13,9 +13,46 @@ describe("convertJSONSchemaToArgument", () => {
   describe("when there's a single type", () => {
     const schema: JSONSchema7 = {
       type: "number",
+      description: "foo bar"
     };
     it("should not throw", () => {
       expect(() => convertJSONSchemaToArgument(schema)).not.toThrowError("Cannot convert JSON Schema to cling Argument with multiple types");
     });
+    it('should return the expected corresponding argument', () => {
+      expect(convertJSONSchemaToArgument(schema)).toStrictEqual({
+        type: 'number',
+        description: 'foo bar'
+      });
+    });
+    describe('array type', () => {
+      const arraySchema: JSONSchema7 = {
+        type: "array",
+        items: {
+          type: 'string'
+        }
+      };
+      it('should return the expected corresponding array argument', () => {
+        expect(convertJSONSchemaToArgument(arraySchema)).toStrictEqual({
+          type: 'array',
+          items: {
+            type: 'string'
+          }
+        });
+      });
+    })
+    describe('string type', () => {
+      describe('enum property', () => {
+        const stringSchema: JSONSchema7 = {
+          type: "string",
+          enum: ['lol']
+        };
+        it('should return the expected corresponding array argument', () => {
+          expect(convertJSONSchemaToArgument(stringSchema)).toStrictEqual({
+            type: 'string',
+            enum: ['lol']
+          });
+        });
+      })
+    })
   });
 });
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,24 +1,55 @@
 import { JSONSchema7 } from "json-schema";
-import { Argument } from "@cling/parser";
+import type { Argument, BaseArgument, TypeName } from "@cling/parser/dist/types";
 import deref from "json-schema-deref-sync";
+import { pick, merge } from 'ramda'
+import {Overwrite, Required} from 'utility-types'
+
+type RequiredProp <TObject extends Record<any, any>, TProp extends string> = {
+  [Prop in keyof TObject]: Prop extends TProp ? NonNullable<TObject[Prop]> : TObject[Prop]
+}
+
+type Lol = RequiredProp<{
+  lol?: string
+}, 'lol'>
+
+const convertBaseArgumentProperties = (jsonSchema: JSONSchema7): BaseArgument<any> => {
+  if (Array.isArray(jsonSchema.type)) {
+    throw new Error(
+      "Cannot convert JSON Schema to cling Argument with multiple types"
+    );
+  }
+  if (!jsonSchema.type) {
+    throw new Error("Cannot convert a JSON Schema without a type");
+  }
+  if (jsonSchema.type === 'object') {
+    throw new Error("Cannot convert a JSON Schema with an `object` type");
+  }
+
+  const commonKeys = ['type', 'description', 'format'] as const
+
+  const validatedSchema = jsonSchema as Required<JSONSchema7, 'type'>
+
+  return pick(commonKeys, validatedSchema)
+}
 
 export const convertJSONSchemaToArgument = (
   jsonSchema: JSONSchema7
 ): Argument => {
   const dereferencedSchema = deref(jsonSchema);
-  if (Array.isArray(dereferencedSchema.type)) {
-    throw new Error(
-      "Cannot convert JSON Schema to cling Argument with multiple types"
-    );
+  const baseArgument = convertBaseArgumentProperties(jsonSchema)
+  /* "Enumable" types */
+  if(jsonSchema.type! === 'string') {
+    return merge(baseArgument, pick(['enum'], jsonSchema))
   }
-  if (!dereferencedSchema.type) {
-    throw new Error("Cannot convert a JSON Schema without a type");
+  if(jsonSchema.type! === 'number') {
+    return merge(baseArgument, pick(['enum'], jsonSchema))
   }
-  if (dereferencedSchema.type === 'object') {
-    throw new Error("Cannot convert a JSON Schema with an `object` type");
+  if(jsonSchema.type! === 'integer') {
+    return merge(baseArgument, pick(['enum'], jsonSchema))
   }
-  return {
-    type: dereferencedSchema.type,
-    description: dereferencedSchema.description,
-  };
+  /* Arrays */
+  if(jsonSchema.type! === 'array') {
+    return merge(baseArgument, pick(['items'], jsonSchema))
+  }
+  return baseArgument
 };
